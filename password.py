6f64cf4a4afa273bd9501b74122a9f1d61d204df
manaskumar01 <manas5226j@gmail.com>
2024-02-16 20:43:23 +0530

    Add password generator to repo
    I made a password generator which is  useful tool that generates strong and
    random passwords for users. This project aims to create a
    password generator application using Python, allowing users to
    specify the length and complexity of the password.

---
+++ password.py
@@ -0,0 +1,9 @@
+import random
+import string
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+password_length = int(input("Enter the length of the password: "))
+generated_password = generate_password(password_length)
+print("Generated Password:", generated_password)
